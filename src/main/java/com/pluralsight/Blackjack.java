111b919327f0320984c16beaa1545ed3182138b9
Nauman <naumansqb@gmail.com>
2025-10-23 09:45:39 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/Blackjack.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Blackjack {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+    }
+}
